702ebc510cdaff90b285d9ed96d99409954fa3ec
Driss Belaroussi <Driss Belaroussi>
2017-05-13 13:02:20 +0200

    Revert "AppliRV v.1.1"
    This reverts commit 6c0ac7994d52ddb5f20a5e1f66d66445a3bfe68f.

--- CP_DB_FraisApp2/CP_DB_FraisApp/README.md
+++
@@ -1,4 +0,0 @@
-mon_projet
-==========
-
-A Symfony project created on October 11, 2016, 11:23 am.
--- CP_DB_FraisApp2/CP_DB_FraisApp/src/FraisBundle/FraisBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FraisBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FraisBundle extends Bundle
-{
-}
